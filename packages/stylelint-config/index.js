5726433a2fd1802dad196b038aa324a45dc50d3c
zdydmn <fengjiaqi0426@163.com>
2025-01-08 16:57:57 +0800

    feat: 创建stylelint 规范

---
+++ packages/stylelint-config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    defaultSeverity: "warning",
+    /** 
+     * Stylistic issues rules
+     * @link https://stylelint.io/user-guide/rules/#stylistic-issues
+     */
+    rules: {
+        indentation: 2 // 【强制】缩进使用 2 个空格做为一个缩进层级, 
+    }
+}
